a1beae39abd1a81d548bf1075552009a35033d25
Pedro <orsomethingelse@gmail.com>
2020-11-04 20:18:12 -0600

    modified models with migrations

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -9,6 +9,8 @@ class Api::MoviesController < ApplicationController
       title: params["title"],
       year: params["year"],
       plot: params["plot"],
+      english: params["english"],
+      director: params["director"],
     })
     @movie.save
     render "show.json.jb"
@@ -26,6 +28,8 @@ class Api::MoviesController < ApplicationController
     @movie.title = params["title"] || @movie.title
     @movie.year = params["year"] || @movie.year
     @movie.plot = params["plot"] || @movie.plot
+    @movie.english = params["english"] || @movie.english
+    @movie.director = params["director"] || @movie.director
     render "show.json.jb"
   end
 
