4d1ef12ba527f3f1fc8d6dcd1ab343828709ec2d
Pedro <orsomethingelse@gmail.com>
2020-11-09 20:18:45 -0600

    has many associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :actors
   validates :title, presence: true, length: { in: 2..20 }
   validates :year, presence: true, numericality: { only_integer: true }
   validates :plot, presence: true, length: { in: 10..200 }
